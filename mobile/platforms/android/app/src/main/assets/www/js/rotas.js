6b695c4559efa842218df648275adc369044bd3a
jvitor_ferraz <jvitor.ferraz14@gmail.com>
2018-12-01 06:06:40 -0200

    v 0.0.4 - Construção do caminho

--- mobile/platforms/android/app/src/main/assets/www/js/rotas.js
+++ mobile/platforms/android/app/src/main/assets/www/js/rotas.js
@@ -6,6 +6,115 @@ rotas = [
             pageInit: (e,page) => {
                 
             }
-        }
+        },
+        
     },
+    {
+        name:'movimento',
+        path:'/movimento/',
+        url:'./pages/movimento.html',
+        on: {
+            pageInit: (e,page) => {
+
+                    var pontos = [];
+
+                    plugin.google.maps.environment.setEnv({
+                        'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyA3WECrzdOEqFDthJAti_ytD9BnPBIMZXc',
+                        'API_KEY_FOR_BROWSER_DEBUG': ''
+                    });
+                    
+                    var div = document.getElementById("map_canvas");
+                    // Create a Google Maps native view under the map_canvas div.
+                    var map = plugin.google.maps.Map.getMap(div);
+
+                    var lat_ant = null;
+                    var lon_ant = null;
+
+                    function calcCrow(lat1, lon1, lat2, lon2){
+
+                        if(lat2 == null || lon2 == null){
+                            return 0;
+                        }
+
+                        var R = 6371; // km
+                        var dLat = toRad(lat2-lat1);
+                        var dLon = toRad(lon2-lon1);
+                        var lat1 = toRad(lat1);
+                        var lat2 = toRad(lat2);
+                        
+                        var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+                        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+                        var d = (R * c) * 1000;
+                        
+                        return parseInt(d);
+                    }
+
+                    function toRad(value) {
+                          return value * Math.PI / 180;
+                    }
+                    function velocidade(speed){
+                        var velocidade = (speed!=null? speed : 0) * 3.6;
+                        velocidade = Number(velocidade -velocidade%1).toFixed(0);
+                        return velocidade;
+                    }
+                    function watch_success(position) {
+                        console.log( JSON.stringify(position.coords) );
+
+                        setCurrentPosition(position);
+
+                        console.log(`${position.coords.latitude} == ${position.coords.longitude} || ${lat_ant} == ${lon_ant}`);
+                        var distancia = calcCrow(position.coords.latitude, position.coords.longitude, lat_ant, lon_ant);
+
+                        console.log(distancia);
+
+                        lat_ant = position.coords.latitude;
+                        lon_ant = position.coords.longitude;
+                    }
+
+                    function watch_error(error) {
+                        alert('code: '    + error.code    + '\n' +
+                            'message: ' + error.message + '\n');
+                    }
+                
+                    var watchID = navigator.geolocation.watchPosition(watch_success, watch_error, { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true });
+
+                    function setCurrentPosition(pos) {
+                        pontos.push({lat: pos.coords.latitude, lng: pos.coords.longitude});
+                        if(pontos.length == 1){
+                            map.animateCamera({
+                                target: {lat: pos.coords.latitude, lng: pos.coords.longitude},
+                                zoom: 30,
+                                tilt: 60,
+                                bearing: 140,
+                                duration: 3000
+                            }, function() {
+                                //alert("Camera target has been changed");
+                            });
+                        }
+                            
+                        map.addPolyline({
+                            'points': pontos,
+                            'color' : '#EE1133',
+                            'width': 6,
+                            'geodesic': true
+                        });
+
+                        map.addMarker({
+                            'position': new plugin.google.maps.LatLng(
+                                                    pos.coords.latitude,
+                                                    pos.coords.longitude
+                                                ),
+                        }, function(marker) {
+                            currentPositionMarker = marker;
+                        });
+
+                        map.setCenter(new plugin.google.maps.LatLng(
+                                                    pos.coords.latitude,
+                                                    pos.coords.longitude
+                                                ));
+                    }
+                
+            }
+        }
+    }
 ];
